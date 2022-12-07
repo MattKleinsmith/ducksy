2034bccfe61480cf873f975e8ff829932648c303
Huishi <anhuishi95@gmail.com>
2022-12-06 16:40:25 -0800

    Update seed data

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.schema import Column
-from sqlalchemy.orm import ForeignKey, relationship
-from sqlalchemy.types import Integer, String, DateTime, Float
+from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.types import Integer, String, DateTime, Float, Text
 from flask_login import UserMixin
 #  a crypto library that came with Flask
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,39 +9,22 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 
-class Product(db.Model):
-    __tablename__ = "products"
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    name = Column(String(140), nullable=False)
-    price = Column(Float(asdecimal=True), nullable=False)
-    description = Column(String(5000), nullable=False)
-
-    user = relationship("Product", back_populates="products")
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "name": self.name,
-            "price": self.price,
-            "description": self.description
-        }
-
-
 class User(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    display_name = Column(String(255), nullable=False)
+    display_name = Column(String(100), nullable=False)
     email = Column(String(100), unique=True)
-    profile_picture = Column(String(255))
-    hashed_password = Column(String(255), nullable=False)
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=False)
+    profile_picture = Column(Text)
+    hashed_password = Column(Text, nullable=False)
+    created_at = Column(DateTime(timezone=True),
+                        server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True),
+                        server_default=func.now(), onupdate=func.now(),
+                        nullable=False)
 
     products = relationship("Product", back_populates="user")
+    orders = relationship("Order", back_populates="user")
 
     @property
     def password(self):
@@ -60,4 +43,42 @@ class User(db.Model, UserMixin):
             "display_name": self.display_name,
             "email": self.email,
             "profile_picture": self.profile_picture,
+        }
+
+
+class Product(db.Model):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    name = Column(String(140), nullable=False)
+    price = Column(Float(asdecimal=True), nullable=False)
+    description = Column(Text, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
+    user = relationship("User", back_populates="products")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "name": self.name,
+            "price": self.price,
+            "description": self.description
+        }
+
+
+class Order(db.Model):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    user = relationship("User", back_populates="orders")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
         }
--- database.py
+++ database.py
@@ -1,14 +1,15 @@
 from app import app
 from app.models import db, User
 from dotenv import load_dotenv
+from datetime import datetime
 load_dotenv()
 
 with app.app_context():
     db.drop_all()
     db.create_all()
     user1 = User(display_name="Margot",
-                 email="test1@user.io", password="password")
+                 email="test1@user.io", password="password", created_at=datetime(), updated_at=datetime())
     user2 = User(display_name="Marshall",
-                 email="test2@user.io", password="password")
+                 email="test2@user.io", password="password", created_at=datetime(), updated_at=datetime())
     db.session.add_all([user1, user2])
     db.session.commit()
