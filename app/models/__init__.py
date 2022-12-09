570e15a9ecf8efd14852978b3f9317a52d60b44d
Huishi <anhuishi95@gmail.com>
2022-12-09 12:49:36 -0800

    Migrate orders tables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,4 +3,6 @@ from .user import User
 from .product import Product
 from .product_image import ProductImage
 from .review import Review
+from .order import Order
+from .order_item import OrderItem
 from .db import environment, SCHEMA
