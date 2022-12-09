b0772497dfebadbb68344d564d842b62b5942c60
Huishi <anhuishi95@gmail.com>
2022-12-09 13:57:17 -0800

    Register users bp

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from . import products, reviews, session, orders
+from . import products, reviews, session, orders, users
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
@@ -7,6 +7,7 @@ bp.register_blueprint(session.bp)
 bp.register_blueprint(products.bp)
 bp.register_blueprint(reviews.bp)
 bp.register_blueprint(orders.bp)
+bp.register_blueprint(users.bp)
 
 
 @bp.route("docs")
