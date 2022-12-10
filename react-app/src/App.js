115af16cd3d42fa43afec51d7802009295f97754
JadeVieTran <jadevtran@gmail.com>
2022-12-10 13:57:30 -0600

    Set up purchases route frontend

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,13 +6,14 @@ import Header from "./components/Header/Header";
 import Modals from "./components/Modals/Modals";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
 import ProductGrid from "./components/ProductGrid/ProductGrid";
+import Purchases from "./components/Purchases/Purchase";
 import { restoreUser } from "./store/session";
 
 export default function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(restoreUser())
-  }, [dispatch])
+    dispatch(restoreUser());
+  }, [dispatch]);
 
   return (
     <>
@@ -21,6 +22,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<ProductGrid />} />
         <Route path="/products/:productId" element={<ProductDetails />} />
+        <Route exact path='/purchases_reviews' element={<Purchases />} />
       </Routes>
     </>
   );
