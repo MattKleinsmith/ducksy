b873597fe09381b34d49c5b3e75c91e6320f3b36
JadeVieTran <jadevtran@gmail.com>
2022-12-10 03:22:42 -0600

    Add reviewimage model

--- app/models/review_image.py
+++ app/models/review_image.py
@@ -12,36 +12,26 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class Review(db.Model):
-    __tablename__ = "reviews"
+class ReviewImage(db.Model):
+    __tablename__ = "review_images"
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = Column(Integer, primary_key=True)
-    customer_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    shop_id = Column(Integer, ForeignKey('users.id'))
-    product_id = Column(Integer, ForeignKey('products.id'), nullable=False)
-    rating = Column(Integer, nullable=False)
-    review = Column(VARCHAR(840), nullable=False)
-
+    review_id = Column(Integer, ForeignKey("reviews.id"), nullable=False)
+    url = Column(TEXT, nullable=False)
     created_at = Column(DateTime(timezone=True),
                         server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True),
                         server_default=func.now(), onupdate=func.now(),
                         nullable=False)
 
-    customer = relationship("User", foreign_keys=customer_id)
-    shop = relationship("User", foreign_keys=shop_id)
-    product = relationship("Product", back_populates="reviews")
-    review_images = relationship("ReviewImage", back_populates="review")
+    review = relationship("Review", back_populates="review_images")
 
     def to_dict(self):
         return {
             "id": self.id,
-            "customer_id": self.customer_id,
-            "shop_id": self.shop_id,
-            "product_id": self.product_id,
-            "rating": self.rating,
-            "review": self.review
+            "review_id": self.review_id,
+            "url": self.url
         }
