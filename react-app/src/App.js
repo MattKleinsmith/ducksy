c2269c5c566abc4b967dafef8e101faea31f91eb
JadeVieTran <jadevtran@gmail.com>
2022-12-12 11:36:28 -0600

    Change name from purchases to orders and add orders store

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,7 +7,7 @@ import Modals from "./components/Modals/Modals";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
 import ProductGrid from "./components/ProductGrid/ProductGrid";
 import { restoreUser } from "./store/session";
-import Purchases from "./components/Purchases/Purchases";
+import Orders from "./components/Orders/Orders";
 
 export default function App() {
   const dispatch = useDispatch();
@@ -22,7 +22,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<ProductGrid />} />
         <Route path="/products/:productId" element={<ProductDetails />} />
-        <Route exact path='/purchases_reviews' element={<Purchases />} />
+        <Route exact path='/purchases_reviews' element={<Orders />} />
       </Routes>
     </>
   );
--- react-app/src/components/Purchases/Purchases.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Purchases() {
-    return (
-        <div>purchases</div>
-    );
-}
