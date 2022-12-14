7ccffbe6f6be8dbf77f5ac28fbfd144730f814eb
Huishi <anhuishi95@gmail.com>
2022-12-13 16:17:50 -0800

    Add CartList

--- react-app/src/components/ShoppingCart/CartList/CartList.js
+++ react-app/src/components/ShoppingCart/CartList/CartList.js
@@ -1,18 +1,27 @@
+import { useSelector } from "react-redux"
+import { NavLink } from "react-router-dom"
 import CartItem from "./CartItem/CartItem"
+import './CartList.css'
 
+export function CartList({ carts }) {
+    const user = useSelector(state => state.session.user)
+    const products = useSelector(state => state.products)
+    const current_cart = user ? carts[user.id] : carts["guest"]
+    const cart_items = Object.entries(current_cart)
 
-export function CartList({ cart }) {
-    let cart_entries = cart ? Object.entries(cart) : null;
-
-    console.log(cart_entries)
     return (
         <>
-            {/* {cart_entries
-                && cart_entries.map(([product_id, amount], i) => <CartItem product_id={Number(product_id)} amount={amount} key={i} />)}
-            {!cart_entries
-                && <div>
-                    <h1>Your cart is empty</h1>
-                </div>} */}
+            {Object.keys(products).length > 0 &&
+                <div className="CartList">
+                    {cart_items.length > 0
+                        && cart_items.map(([product_id, amount], i) => <CartItem product={products[product_id]} amount={amount} key={i} />)}
+                    {cart_items.length === 0
+                        && <div>
+                            <h1>Your cart is empty</h1>
+                            <NavLink to={'/'}>Discover something unique to fill it up</NavLink>
+                        </div>}
+                </div>
+            }
         </>
     )
 }
