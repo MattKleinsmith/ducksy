41be603478d0051d64cf512de3e07f8e13f3f1a0
Huishi <anhuishi95@gmail.com>
2022-12-13 16:16:47 -0800

    Add CartSummary Component

--- react-app/src/components/ShoppingCart/CartSummary/CartSummary.js
+++ react-app/src/components/ShoppingCart/CartSummary/CartSummary.js
@@ -1,28 +1,32 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
-
+import './CartSummary.css'
 
 
 export default function CartSummary({ carts }) {
     const navigate = useNavigate();
     const user = useSelector(state => state.session.user)
-    console.log('CartSummary', carts)
-
     const totalItems = (carts) => {
         const current_cart = user ? carts[user.id] : carts["guest"]
-        const itemNum = Object.values(current_cart).reduce((total, num) => total += num, 0);
-        return itemNum;
+        const total = Object.values(current_cart).reduce((total, num) => total += num, 0);
+        return total;
     };
+    const itemNum = totalItems(carts)
+    console.log(itemNum);
 
     return (
-        <div className="cartSummary">
-            <div>
-                <h1>{totalItems(carts)} items in your cart</h1>
-            </div>
-            <div>
-                <button onClick={() => navigate('/')}>
-                    Keep shopping</button>
-            </div>
-        </div>
+        <>
+            {itemNum &&
+                <div className="cartSummary">
+                    <div>
+                        <h1>{totalItems(carts)} items in your cart</h1>
+                    </div>
+                    <div>
+                        <button onClick={() => navigate('/')}>
+                            Keep shopping</button>
+                    </div>
+                </div>
+            }
+        </>
     )
 };
