1f29d052ec58279fdff4c92bc1e080ad8cd7d76c
Huishi <anhuishi95@gmail.com>
2022-12-14 09:49:16 -0800

    Add getCarts thunk action

--- react-app/src/store/shoppingCart.js
+++ react-app/src/store/shoppingCart.js
@@ -1,19 +1,44 @@
 import { csrfFetch } from './csrf';
 
+export const postCarts = (carts) => {
+    window.localStorage.setItem('ducksyCarts', JSON.stringify(carts));
+}
+
 const GET_CARTS = 'shoppingCart/GET_CARTS'
 
-export const getCarts = () => async dispatch => {
-    const carts = window.localStorage.getItem('ducksy');
+export const getCarts = (user = null) => async dispatch => {
+    const cart_storage = window.localStorage.getItem('ducksyCarts');
+    const carts = cart_storage ? JSON.parse(cart_storage) : {};
+    if (!carts["guest"]) {
+        carts["guest"] = {};
+    }
+    if (user) {
+        if (!carts[user.id]) carts[user.id] = {}
+    }
+    postCarts(carts)
+    dispatch({ type: GET_CARTS, carts })
+    return carts
+}
+
+// const MERGE_CARTS = 'shoppingCart/MERGE_CARTS'
+export const mergeCarts = (user_id) => async dispatch => {
+    carts = dispatch(getCarts())
+    // Merge guest cart to log-in user cart
+    carts[user_id] = Object.assign(carts[user_id], carts["guest"]);
+    //  Clear guest cart
+    carts["guest"] = {};
     dispatch({ type: GET_CARTS, carts })
 }
 
-export const mergeCarts = () => async disptach => {
-
+export const deleteItem = (user_id, product_id) => async dispatch => {
+    carts = dispatch(getCarts())
+    delete carts[user_id][product_id]
+    postCarts()
+    dispatch({ type: GET_CARTS, carts })
 }
 
-const MERGE_CARTS = 'shoppingCart/MERGE_CARTS'
 const UPDATE_CART = 'shoppingCart/UPDATE_CART'
-const DELETE_CART = 'shoppingCart/UPDATE_CART'
+const DELETE_CART = 'shoppingCart/DELETE_CART'
 
 
 
@@ -23,6 +48,7 @@ export default function ShoppingCartReducer(state = initialState, action) {
     switch (action.type) {
         case GET_CARTS:
             return action.carts
-
+        default:
+            return state
     }
 }
