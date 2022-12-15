867dedd5fc7714e2132fddeedc01a901829ad7b8
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-15 14:18:15 -0600

    Fix Postman search by category

--- react-app/src/components/Header/SearchBar/SearchBar.js
+++ react-app/src/components/Header/SearchBar/SearchBar.js
@@ -1,8 +1,11 @@
 import styles from "./SearchBar.module.css";
 import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getProductsByCategory } from "../../../store/productsByCategory";
 
 export default function SearchBar() {
     const [isSearchBarActive, setIsSearchBarActive] = useState(false);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (!isSearchBarActive) return;
@@ -13,10 +16,14 @@ export default function SearchBar() {
         return () => document.removeEventListener("click", setSearchBarInactive);
     }, [isSearchBarActive]);
 
+    const handleSearch = () => {
+        dispatch(getProductsByCategory())
+    }
+
     return (
         <div className={styles.searchBarWrapper} onClick={() => setIsSearchBarActive(true)}>
             <input type="text" className={styles.searchBar} placeholder="Search for anything" />
-            <div className={`${styles.iconWrapperBase} ${isSearchBarActive && styles.iconWrapperActive}`}>
+            <div onClick={handleSearch} className={`${styles.iconWrapperBase} ${isSearchBarActive && styles.iconWrapperActive}`}>
                 <i className={`fa-solid fa-magnifying-glass ${isSearchBarActive ? styles.iconActive : styles.icon}`} />
             </div>
         </div>
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -9,6 +9,7 @@ import uiReducer from "./ui";
 import ReviewDetailsReducer from "./reviewDetails";
 import productReviewsReducer from "./productReviews";
 import ShoppingCartReducer from "./shoppingCart";
+import productsByCategoryReducer from "./productsByCategory";
 
 const rootReducer = combineReducers({
     session: sessionReducer,
@@ -20,6 +21,7 @@ const rootReducer = combineReducers({
     buyerReviews: buyerReviewsReducer,
     reviewDetails: ReviewDetailsReducer,
     shoppingCarts: ShoppingCartReducer,
+    productsByCategory: productsByCategoryReducer,
 });
 
 let enhancer;
