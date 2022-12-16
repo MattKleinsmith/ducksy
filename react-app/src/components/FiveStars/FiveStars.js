eb9bae6bb6c2419e0fb9761851972b4d303e7b7f
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-16 16:46:04 -0600

    Fix React warning ==

--- react-app/src/components/FiveStars/FiveStars.js
+++ react-app/src/components/FiveStars/FiveStars.js
@@ -5,7 +5,7 @@ export default function FiveStars({ rating }) {
     const halfStar = (rating - fullStarNum) * 10
     return (<>
         {[...Array(fullStarNum).keys()].map(_ => (<i className="fa-solid fa-star FiveStars" />))}
-        {fullStarNum < 5 && ((halfStar > 5 || halfStar == 0) ?
+        {fullStarNum < 5 && ((halfStar > 5 || halfStar === 0) ?
             (<i className="fa-solid fa-star FiveStars" />) :
             (<i className="fa-solid fa-star-half FiveStars"></i>))}
     </>);
