124ed6414d3078bf9e3a6c938bae2baefadbd593
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-16 13:35:02 -0600

    Seed gifts and wedding

--- app/seeds/scrape.js
+++ app/seeds/scrape.js
@@ -1,7 +1,7 @@
 result = `
     product = Product(
         seller=anna,
-        categories=[personalized_gift],
+        categories=[wedding],
         name="${document.querySelector(".wt-text-body-03.wt-line-height-tight.wt-break-word").innerText}",
         price="${document.querySelectorAll(".wt-text-title-03.wt-mr-xs-1")[0].innerText.replace('+', '').replace("$", '')}",
         description="${document.querySelector(".wt-text-body-01.wt-break-word").innerText.split('\n')[0].replaceAll('"', "'")}"
@@ -22,8 +22,7 @@ users = ["brian", "caitlynn", "derrik", "elizabeth"]
 ratings.forEach((rating, i) => {
     result += `
         Review(
-            author=${users[i]},
-            seller=anna,
+            buyer=${users[i]},
             product=product,
             rating=${rating},
             review="${reviews[i].replaceAll('"', "'")}"
