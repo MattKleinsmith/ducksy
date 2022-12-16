bcbe6d5e5055aa99c5af6122d67c3870891ba97b
JadeVieTran <jadevtran@gmail.com>
2022-12-15 22:48:04 -0600

    Styling category homepage

--- react-app/src/components/Homepage/Homepage.js
+++ react-app/src/components/Homepage/Homepage.js
@@ -14,19 +14,34 @@ export default function Homepage() {
             <div className={styles.welcome}>{currentUser ?
                 <div>Welcome back, {currentUser.display_name}</div> :
                 <div>Find things you'll love. Support independent sellers. Only on Ducksy.</div>}
-                <div>
-                    <div>
-                        <img href='' alt=''></img>
-                        <div>Gift</div>
+                <div className={styles.categoryWrapper}>
+                    <div className={styles.imageWrapper}>
+                        <img className={styles.categoryImage} src='https://i.etsystatic.com/17766621/r/il/16e3f0/4236608158/il_794xN.4236608158_aztc.jpg' alt='img'></img>
+                        <div className={styles.categoryName}>Personalized Gifts</div>
                     </div>
-                    <div>Wedding</div>
-                    <div>Personalized Gifts</div>
-                    <div>Home Decor</div>
-                    <div>Bag</div>
-                    <div>Boyfriend</div>
+                    <div className={styles.imageWrapper}>
+                        <img className={styles.categoryImage} src='https://i.etsystatic.com/8617622/r/il/0c6972/2264040092/il_794xN.2264040092_5oim.jpg' alt='img'></img>
+                        <div className={styles.categoryName}>Wedding</div>
+                    </div>
+                    <div className={styles.imageWrapper}>
+                        <img className={styles.categoryImage} src='https://i.etsystatic.com/27235619/r/il/b029b4/2847834009/il_794xN.2847834009_nd9k.jpg' alt='img'></img>
+                        <div className={styles.categoryName}>Home Decor</div>
+                    </div>
+                    <div className={styles.imageWrapper}>
+                        <img className={styles.categoryImage} src='https://i.etsystatic.com/17305851/r/il/95b009/3398360096/il_794xN.3398360096_rpcp.jpg' alt='img'></img>
+                        <div className={styles.categoryName}>Toy</div>
+                    </div>
+                    <div className={styles.imageWrapper}>
+                        <img className={styles.categoryImage} src='https://i.etsystatic.com/21515874/r/il/70440b/2916023872/il_794xN.2916023872_agns.jpg' alt='img'></img>
+                        <div className={styles.categoryName}>Bag</div>
+                    </div>
+                    <div className={styles.imageWrapper}>
+                        <img className={styles.categoryImage} src='https://i.etsystatic.com/20811748/r/il/824d8a/3906405268/il_794xN.3906405268_aubi.jpg' alt='img'></img>
+                        <div className={styles.categoryName}>Boyfriend</div>
+                    </div>
                 </div>
             </div>
-            <div className={styles.welcome}></div>
+            <div className={styles.welcome2}></div>
             <div className={styles.wrapper}>
                 <div className={styles.recentViewed}>Recently viewed & more</div>
                 <div className={styles.homepage}>
