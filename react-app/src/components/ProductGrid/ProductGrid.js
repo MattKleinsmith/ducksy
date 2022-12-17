6b0bf41c349324431fabcc446ea34a3c1825f739
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-16 18:53:20 -0600

    Limit products on Homepage

--- react-app/src/components/ProductGrid/ProductGrid.js
+++ react-app/src/components/ProductGrid/ProductGrid.js
@@ -13,7 +13,7 @@ export default function ProductGrid({ isHomepage = false }) {
     const productsBySearch = useSelector(state => Object.values(state.productsBySearch));
     let products = productsBySearch.length ? productsBySearch : productsAll;
     if (categoryName) products.reverse()
-    if (isHomepage) products = products.slice(10)
+    if (isHomepage) products = products.slice(10, 30)
 
     useEffect(() => {
         if (categoryName) dispatch(getProductsByCategory(categoryName));
