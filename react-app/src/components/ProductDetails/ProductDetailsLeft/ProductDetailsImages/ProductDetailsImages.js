6c0c7f7083c8a9efe3412f6bec3231b195cb87de
JadeVieTran <jadevtran@gmail.com>
2022-12-17 15:00:58 -0600

    Fix ProductDetailsImages buttons

--- react-app/src/components/ProductDetails/ProductDetailsLeft/ProductDetailsImages/ProductDetailsImages.js
+++ react-app/src/components/ProductDetails/ProductDetailsLeft/ProductDetailsImages/ProductDetailsImages.js
@@ -1,16 +1,24 @@
 import styles from "./ProductDetailsImages.module.css";
+import { useState } from "react";
 
 export default function ProductDetailsImages({ product }) {
+    const [url, setUrl] = useState("");
+
+    const onClickHandler = (productImageId) => {
+        const image = product.product_images.find(image => image.id === productImageId);
+        setUrl(image.url);
+    };
+
     return (
         <div className={styles.ProductDetailsImagesWrapper}>
             <div className={styles.moreImagesWrapper}>
-                {product.product_images.map(img =>
-                    <button className={styles.moreiImagesBtn}>
-                        <img src={img.url} alt="ProductDetailsImages" className={styles.moreImages} />
-                    </button>)}
+                {product.product_images.map(product_image =>
+                    <div className={styles.moreiImagesBtn} onClick={() => onClickHandler(product_image.id)}>
+                        <img src={product_image.url} alt="ProductDetailsImages" className={styles.moreImages} />
+                    </div>)}
             </div>
             <div className={styles.defaultImageWrapper}>
-                <img src={product.preview_image} alt="ProductDetailsImages" className={styles.defaultImage} />
+                <img src={url} alt="ProductDetailsImages" className={styles.defaultImage} />
             </div>
         </div>
     );
