a27ae77686a28ec593242920e18f5536ac658617
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-17 16:45:43 -0600

    Define UnitedStatesIcon

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,17 +1,26 @@
 import styles from "./Footer.module.css";
+import UnitedStatesIcon from "./UnitedStatesIcon";
 
 export default function Footer() {
     return (
         <div className={styles.wrapper}>
-            <div className={styles.line}></div>
-            <div className={styles.footer}>
-                <div className={styles.left}>
-                    United States   |   English (US)   |   $ (USD)</div>
-                <div className={styles.right}>
-                    <div>Developers:</div>
-                    <a href="https://github.com/jadevie">Jade Tran</a>
-                    <a href="https://github.com/huishi329">Huishi An</a>
-                    <a href="https://github.com/MattKleinsmith">Matt Kleinsmith</a>
+            <div className={styles.subWrapper}>
+                <div className={styles.line}></div>
+                <div className={styles.footer}>
+                    <div className={styles.left}>
+                        <UnitedStatesIcon />
+                        <div>United States</div>
+                        <div>|</div>
+                        <div>English (US)</div>
+                        <div>|</div>
+                        <div>$ (USD)</div>
+                    </div>
+                    <div className={styles.right}>
+                        <div>Developers:</div>
+                        <a href="https://github.com/jadevie">Jade Tran</a>
+                        <a href="https://github.com/huishi329">Huishi An</a>
+                        <a href="https://github.com/MattKleinsmith">Matt Kleinsmith</a>
+                    </div>
                 </div>
             </div>
         </div>
