319a627c6c685146f6169aeb68da986d98e756a2
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-16 20:05:09 -0600

    Fix star count in FiveStars in ProductDetails

--- react-app/src/components/FiveStars/FiveStars.js
+++ react-app/src/components/FiveStars/FiveStars.js
@@ -1,12 +1,15 @@
 import "./FiveStars.css"
 
 export default function FiveStars({ rating }) {
-    const fullStarNum = Math.round(rating)
-    const halfStar = (rating - fullStarNum) * 10
+    const fullStarNum = Math.floor(rating);
+    const partialStar = rating % 1;
     return (<>
-        {[...Array(fullStarNum).keys()].map(_ => (<i className="fa-solid fa-star FiveStars" />))}
-        {halfStar !== 0 && (halfStar > 5 ?
-            (<i className="fa-solid fa-star FiveStars" />) :
-            (<i className="fa-solid fa-star-half FiveStars"></i>))}
+        {[...Array(fullStarNum)].map(_ => (<i className="fa-solid fa-star FiveStars" />))}
+        {partialStar !== 0 &&
+            (partialStar > 0.5 ?
+                <i className="fa-solid fa-star FiveStars" />
+                :
+                <i className="fa-solid fa-star-half FiveStars" />)
+        }
     </>);
 }
