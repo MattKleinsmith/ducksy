e58ac27c90442fd89546e2fd5bd660f24ac2806b
Huishi <anhuishi95@gmail.com>
2022-12-17 13:48:04 -0800

    Style Product Grid

--- react-app/src/components/FiveStars/FiveStars.js
+++ react-app/src/components/FiveStars/FiveStars.js
@@ -4,8 +4,8 @@ export default function FiveStars({ rating }) {
     const fullStarNum = Math.floor(rating);
     const partialStar = rating % 1;
     const lastClass = partialStar > 0.5 ? "" : "-half";
-    return (<>
-        {[...Array(fullStarNum)].map(_ => <i className="fa-solid fa-star FiveStars" />)}
-        {partialStar !== 0 && <i className={`fa-solid fa-star${lastClass} FiveStars`} />}
-    </>);
+    return (<div className="FiveStarsDiv">
+        {[...Array(fullStarNum)].map(_ => <i className="fa-solid fa-star FiveStar" />)}
+        {partialStar !== 0 && <i className={`fa-solid fa-star${lastClass} FiveStar`} />}
+    </div>);
 }
