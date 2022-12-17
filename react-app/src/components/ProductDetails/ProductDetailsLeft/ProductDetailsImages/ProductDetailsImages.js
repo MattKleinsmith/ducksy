54c774707f70e82c2cc7d749c25e0d7a9944725c
JadeVieTran <jadevtran@gmail.com>
2022-12-17 01:01:05 -0600

    Style layout for detail photos

--- react-app/src/components/ProductDetails/ProductDetailsLeft/ProductDetailsImages/ProductDetailsImages.js
+++ react-app/src/components/ProductDetails/ProductDetailsLeft/ProductDetailsImages/ProductDetailsImages.js
@@ -1,16 +1,17 @@
 import styles from "./ProductDetailsImages.module.css";
 
 export default function ProductDetailsImages({ product }) {
-    
     return (
         <div className={styles.ProductDetailsImagesWrapper}>
-                <div className={styles.moreImagesWrapper}>
-                    {product.product_images.map(img =>
-                        <img src={img.url} alt="ProductDetailsImages" className={styles.moreImages} />)}
-                </div>
-                <div className={styles.defaultImageWrapper}>
-                    <img src={product.preview_image} alt="ProductDetailsImages" className={styles.defaultImage} />
-                </div>
+            <div className={styles.moreImagesWrapper}>
+                {product.product_images.map(img =>
+                    <button className={styles.moreiImagesBtn}>
+                        <img src={img.url} alt="ProductDetailsImages" className={styles.moreImages} />
+                    </button>)}
+            </div>
+            <div className={styles.defaultImageWrapper}>
+                <img src={product.preview_image} alt="ProductDetailsImages" className={styles.defaultImage} />
+            </div>
         </div>
     );
 }
