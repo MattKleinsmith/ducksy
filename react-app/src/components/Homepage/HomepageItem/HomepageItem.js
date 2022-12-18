b958df6603024b51bd7f6bc2fc7b04fb973564c3
JadeVieTran <jadevtran@gmail.com>
2022-12-17 19:19:01 -0600

    Fix max-width in Homepage module css

--- react-app/src/components/Homepage/HomepageItem/HomepageItem.js
+++ react-app/src/components/Homepage/HomepageItem/HomepageItem.js
@@ -3,11 +3,11 @@ import styles from "./HomepageItem.module.css";
 export default function HomepageItem({ product }) {
 
     return (
-        <div className={styles.itemWrapper}>
-            <div className={styles.item}>
-                <img src={product.preview_image} alt={product.preview_image} onError={(e) => { e.target.src = "/images/placeholder.png"; }} />
-            </div>
-
+        // <div className={styles.itemWrapper}>
+        <div className={styles.item}>
+            <img src={product.preview_image} alt={product.preview_image} onError={(e) => { e.target.src = "/images/placeholder.png"; }} />
         </div>
+
+        // </div>
     );
 }
