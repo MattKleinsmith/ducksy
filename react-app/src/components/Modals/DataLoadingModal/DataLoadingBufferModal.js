c6cdfacc23c87e8f007a4d5839806034d5c98b26
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-12-18 20:09:09 -0600

    Disallow clicking out of loading modal

--- react-app/src/components/Modals/DataLoadingModal/DataLoadingBufferModal.js
+++ react-app/src/components/Modals/DataLoadingModal/DataLoadingBufferModal.js
@@ -1,16 +1,8 @@
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router';
 import { Modal } from '../../../context/Modal';
-import { setDataLoadingModal } from '../../../store/ui';
 import DataLoadingBuffer from './DataLoadingBuffer';
 
 export default function DataLoadingBufferModal() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    return <Modal onClose={() => {
-        dispatch(setDataLoadingModal(false))
-        navigate('/')
-    }}>
+    return <Modal onClose={() => { }}>
         <DataLoadingBuffer />
     </Modal>;
 }
